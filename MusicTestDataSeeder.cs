4a02b83d1b201ec55b2abba341d029abcd74b244
grammophone <emmanuel.liossis@gmail.com>
2026-06-18 13:58:04 -0400

    Added data seeding. Renamed the domain containers to be consistent with IMusicDomainContainer.

---
+++ MusicTestDataSeeder.cs
@@ -0,0 +1,47 @@
+using Grammophone.DataAccess.Tests.Domain;
+
+namespace Grammophone.DataAccess.Tests.Cases
+{
+	/// <summary>
+	/// Seeds the music test domain.
+	/// </summary>
+	public static class MusicTestDataSeeder
+	{
+		#region Public methods
+
+		/// <summary>
+		/// Seed test data into a music domain container.
+		/// </summary>
+		/// <param name="domainContainer">The domain container to seed.</param>
+		public static void Seed(IMusicDomainContainer domainContainer)
+		{
+			var artist = domainContainer.Create<Artist>();
+			artist.Name = TestData.ArtistName;
+
+			var genre = domainContainer.Create<Genre>();
+			genre.Name = TestData.GenreName;
+
+			var album = domainContainer.Create<Album>();
+			album.Name = TestData.AlbumName;
+			album.ReleaseDate = TestData.AlbumReleaseDate;
+			album.Artist = artist;
+			album.Genre = genre;
+
+			var track = domainContainer.Create<Track>();
+			track.Name = TestData.TrackName;
+			track.Number = 1;
+			track.DurationSeconds = 245;
+			track.Album = album;
+			track.Genre = genre;
+
+			domainContainer.Artists.Add(artist);
+			domainContainer.Genres.Add(genre);
+			domainContainer.Albums.Add(album);
+			domainContainer.Tracks.Add(track);
+
+			domainContainer.SaveChanges();
+		}
+
+		#endregion
+	}
+}
