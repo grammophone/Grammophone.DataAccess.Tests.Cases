6f120b6129918d7995d68384b46bb6c74f61f1ae
grammophone <emmanuel.liossis@gmail.com>
2026-06-18 21:31:33 -0400

    Make Include and AsNoTracking work.

--- QueryExtensionTestCases.cs
+++ QueryExtensionTestCases.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Grammophone.DataAccess.QueryExtensions;
+using Grammophone.DataAccess.Tests.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Grammophone.DataAccess.Tests.Cases
@@ -11,12 +16,247 @@ namespace Grammophone.DataAccess.Tests.Cases
 		#region Public methods
 
 		/// <summary>
-		/// Placeholder for provider-independent include tests.
+		/// Tests expression include with a standard terminal method.
 		/// </summary>
 		[TestMethod]
-		[Ignore("Database setup and seed data will be added in the next test iteration.")]
-		public void Include_tests_are_pending()
+		public void Include_expression_with_standard_terminal_loads_collection()
 		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				domainContainer.IsLazyLoadingEnabled = false;
+
+				var album = domainContainer.Albums
+					.Include(a => a.Tracks)
+					.Single(a => a.Name == TestData.AlbumName);
+
+				Assert.IsTrue(
+					domainContainer.Entry(album)
+						.Collection(a => a.Tracks)
+						.IsLoaded);
+			}
+		}
+
+		/// <summary>
+		/// Tests expression include with an async terminal method.
+		/// </summary>
+		[TestMethod]
+		public async Task Include_expression_with_async_terminal_loads_collection()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				domainContainer.IsLazyLoadingEnabled = false;
+
+				var album = await domainContainer.Albums
+					.Include(a => a.Tracks)
+					.SingleAsync(a => a.Name == TestData.AlbumName);
+
+				Assert.IsTrue(
+					domainContainer.Entry(album)
+						.Collection(a => a.Tracks)
+						.IsLoaded);
+			}
+		}
+
+		/// <summary>
+		/// Tests string include with a standard terminal method.
+		/// </summary>
+		[TestMethod]
+		public void Include_string_with_standard_terminal_loads_collection()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				domainContainer.IsLazyLoadingEnabled = false;
+
+				var album = domainContainer.Albums
+					.Include(nameof(Album.Tracks))
+					.Single(a => a.Name == TestData.AlbumName);
+
+				Assert.IsTrue(
+					domainContainer.Entry(album)
+						.Collection(a => a.Tracks)
+						.IsLoaded);
+			}
+		}
+
+		/// <summary>
+		/// Tests string include with an async terminal method.
+		/// </summary>
+		[TestMethod]
+		public async Task Include_string_with_async_terminal_loads_collection()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				domainContainer.IsLazyLoadingEnabled = false;
+
+				var album = await domainContainer.Albums
+					.Include(nameof(Album.Tracks))
+					.SingleAsync(a => a.Name == TestData.AlbumName);
+
+				Assert.IsTrue(
+					domainContainer.Entry(album)
+						.Collection(a => a.Tracks)
+						.IsLoaded);
+			}
+		}
+
+		/// <summary>
+		/// Tests nested eager loading with a standard terminal method.
+		/// </summary>
+		[TestMethod]
+		public void ThenInclude_with_standard_terminal_loads_nested_reference()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				domainContainer.IsLazyLoadingEnabled = false;
+
+				var album = domainContainer.Albums
+					.Include(a => a.Tracks)
+					.ThenInclude(t => t.Genre)
+					.Single(a => a.Name == TestData.AlbumName);
+
+				AssertTracksAndGenresAreLoaded(domainContainer, album);
+			}
+		}
+
+		/// <summary>
+		/// Tests nested eager loading with an async terminal method.
+		/// </summary>
+		[TestMethod]
+		public async Task ThenInclude_with_async_terminal_loads_nested_reference()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				domainContainer.IsLazyLoadingEnabled = false;
+
+				var album = await domainContainer.Albums
+					.Include(a => a.Tracks)
+					.ThenInclude(t => t.Genre)
+					.SingleAsync(a => a.Name == TestData.AlbumName);
+
+				AssertTracksAndGenresAreLoaded(domainContainer, album);
+			}
+		}
+
+		/// <summary>
+		/// Tests no-tracking queries with a standard terminal method.
+		/// </summary>
+		[TestMethod]
+		public void AsNoTracking_with_standard_terminal_returns_detached_entity()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var album = domainContainer.Albums
+					.AsNoTracking()
+					.Single(a => a.Name == TestData.AlbumName);
+
+				Assert.AreEqual(TrackingState.Detached, domainContainer.Entry(album).State);
+			}
+		}
+
+		/// <summary>
+		/// Tests no-tracking queries with an async terminal method.
+		/// </summary>
+		[TestMethod]
+		public async Task AsNoTracking_with_async_terminal_returns_detached_entity()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var album = await domainContainer.Albums
+					.AsNoTracking()
+					.SingleAsync(a => a.Name == TestData.AlbumName);
+
+				Assert.AreEqual(TrackingState.Detached, domainContainer.Entry(album).State);
+			}
+		}
+
+		/// <summary>
+		/// Tests the portable Like function with a standard terminal method.
+		/// </summary>
+		[TestMethod]
+		public void Like_with_standard_terminal_matches_pattern()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var albums = domainContainer.Albums
+					.Where(a => QueryFunctions.Like(a.Name, "%Integration%"))
+					.ToList();
+
+				Assert.AreEqual(1, albums.Count);
+				Assert.AreEqual(TestData.AlbumName, albums[0].Name);
+			}
+		}
+
+		/// <summary>
+		/// Tests the portable Like function with an async terminal method.
+		/// </summary>
+		[TestMethod]
+		public async Task Like_with_async_terminal_matches_pattern()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var albums = await domainContainer.Albums
+					.Where(a => QueryFunctions.Like(a.Name, "%Integration%"))
+					.ToListAsync();
+
+				Assert.AreEqual(1, albums.Count);
+				Assert.AreEqual(TestData.AlbumName, albums[0].Name);
+			}
+		}
+
+		/// <summary>
+		/// Tests portable date difference with a standard terminal method.
+		/// </summary>
+		[TestMethod]
+		public void DiffDays_with_standard_terminal_filters_results()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var comparisonDate = TestData.AlbumReleaseDate.AddDays(3);
+
+				var albums = domainContainer.Albums
+					.Where(a => QueryFunctions.DiffDays(a.ReleaseDate, comparisonDate) == 3)
+					.ToList();
+
+				Assert.AreEqual(1, albums.Count);
+			}
+		}
+
+		/// <summary>
+		/// Tests portable date difference with an async terminal method.
+		/// </summary>
+		[TestMethod]
+		public async Task DiffDays_with_async_terminal_filters_results()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var comparisonDate = TestData.AlbumReleaseDate.AddDays(3);
+
+				var albums = await domainContainer.Albums
+					.Where(a => QueryFunctions.DiffDays(a.ReleaseDate, comparisonDate) == 3)
+					.ToListAsync();
+
+				Assert.AreEqual(1, albums.Count);
+			}
+		}
+
+		#endregion
+
+		#region Private methods
+
+		private static void AssertTracksAndGenresAreLoaded(IMusicDomainContainer domainContainer, Album album)
+		{
+			Assert.IsTrue(
+				domainContainer.Entry(album)
+					.Collection(a => a.Tracks)
+					.IsLoaded);
+
+			foreach (var track in album.Tracks)
+			{
+				Assert.IsTrue(
+					domainContainer.Entry(track)
+						.Reference(t => t.Genre)
+						.IsLoaded);
+			}
 		}
 
 		#endregion
--- TerminalMethodTestCases.cs
+++ TerminalMethodTestCases.cs
@@ -1,3 +1,7 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Grammophone.DataAccess.QueryExtensions;
+using Grammophone.DataAccess.Tests.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Grammophone.DataAccess.Tests.Cases
@@ -11,12 +15,164 @@ namespace Grammophone.DataAccess.Tests.Cases
 		#region Public methods
 
 		/// <summary>
-		/// Placeholder for provider-independent terminal method tests.
+		/// Tests asynchronous materialization to a list.
 		/// </summary>
 		[TestMethod]
-		[Ignore("Database setup and seed data will be added in the next test iteration.")]
-		public void Terminal_method_tests_are_pending()
+		public async Task ToListAsync_materializes_results()
 		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var albums = await domainContainer.Albums
+					.Where(a => a.Name == TestData.AlbumName)
+					.ToListAsync();
+
+				Assert.AreEqual(1, albums.Count);
+				Assert.AreEqual(TestData.AlbumName, albums[0].Name);
+			}
+		}
+
+		/// <summary>
+		/// Tests asynchronous materialization to an array.
+		/// </summary>
+		[TestMethod]
+		public async Task ToArrayAsync_materializes_results()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var artists = await domainContainer.Artists
+					.Where(a => a.Name == TestData.ArtistName)
+					.ToArrayAsync();
+
+				Assert.AreEqual(1, artists.Length);
+				Assert.AreEqual(TestData.ArtistName, artists[0].Name);
+			}
+		}
+
+		/// <summary>
+		/// Tests asynchronous retrieval of the first result.
+		/// </summary>
+		[TestMethod]
+		public async Task FirstAsync_returns_first_result()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var album = await domainContainer.Albums
+					.OrderBy(a => a.ID)
+					.FirstAsync();
+
+				Assert.AreEqual(TestData.AlbumName, album.Name);
+			}
+		}
+
+		/// <summary>
+		/// Tests asynchronous retrieval of the first matching result or default value.
+		/// </summary>
+		[TestMethod]
+		public async Task FirstOrDefaultAsync_returns_null_for_missing_result()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var album = await domainContainer.Albums
+					.FirstOrDefaultAsync(a => a.Name == "Missing Album");
+
+				Assert.IsNull(album);
+			}
+		}
+
+		/// <summary>
+		/// Tests asynchronous retrieval of the single matching result.
+		/// </summary>
+		[TestMethod]
+		public async Task SingleAsync_returns_single_result()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var artist = await domainContainer.Artists
+					.SingleAsync(a => a.Name == TestData.ArtistName);
+
+				Assert.AreEqual(TestData.ArtistName, artist.Name);
+			}
+		}
+
+		/// <summary>
+		/// Tests asynchronous retrieval of the single matching result or default value.
+		/// </summary>
+		[TestMethod]
+		public async Task SingleOrDefaultAsync_returns_null_for_missing_result()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var genre = await domainContainer.Genres
+					.SingleOrDefaultAsync(g => g.Name == "Missing Genre");
+
+				Assert.IsNull(genre);
+			}
+		}
+
+		/// <summary>
+		/// Tests asynchronous existence checks.
+		/// </summary>
+		[TestMethod]
+		public async Task AnyAsync_returns_true_for_existing_result()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var exists = await domainContainer.Tracks
+					.AnyAsync(t => t.Name == TestData.TrackName);
+
+				Assert.IsTrue(exists);
+			}
+		}
+
+		/// <summary>
+		/// Tests asynchronous count aggregation.
+		/// </summary>
+		[TestMethod]
+		public async Task CountAsync_returns_number_of_matching_results()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var count = await domainContainer.Tracks
+					.CountAsync(t => t.Album.Name == TestData.AlbumName);
+
+				Assert.AreEqual(1, count);
+			}
+		}
+
+		/// <summary>
+		/// Tests asynchronous sum aggregation on a non-empty result set.
+		/// </summary>
+		[TestMethod]
+		public async Task SumAsync_returns_total_for_non_empty_result()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var totalDuration = await domainContainer.Tracks
+					.Where(t => t.Album.Name == TestData.AlbumName)
+					.SumAsync(t => t.DurationSeconds);
+
+				Assert.AreEqual(TestData.TrackDurationSeconds, totalDuration);
+			}
+		}
+
+		/// <summary>
+		/// Tests asynchronous nullable sum aggregation on an empty result set.
+		/// </summary>
+		/// <remarks>
+		/// SQL Server and providers differ on the final empty-set value. Projecting to a nullable value type
+		/// allows the provider to materialize the result instead of failing for a non-nullable value type.
+		/// </remarks>
+		[TestMethod]
+		public async Task Nullable_sum_async_allows_empty_result_set()
+		{
+			using (var domainContainer = CreateDomainContainer())
+			{
+				var totalDuration = await domainContainer.Tracks
+					.Where(t => t.Name == "Missing Track")
+					.SumAsync(t => (int?)t.DurationSeconds);
+
+				Assert.IsTrue(totalDuration == null || totalDuration == 0);
+			}
 		}
 
 		#endregion
